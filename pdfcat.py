4aca3bfdb0d79e434732faef5a7c909da78cdc33
Johan Emmanuel <johanemman243@gmail.com>
2025-10-21 23:02:06 +0530

    Add globbing support

--- pdfcat.py
+++ pdfcat.py
@@ -1,12 +1,16 @@
 import argparse
+import glob
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("output_file", help="name of final merged PDF file")
     parser.add_argument("input_files", help="list of PDF files to merge", nargs='+')
     args = parser.parse_args()
+
+    input_files = [file for arg in args.input_files for file in glob.glob(arg)]
+    input_files.sort()
     
-    return args.output_file, args.input_files
+    return args.output_file, input_files
 
 def main():
     output_file, input_files = parse_args()
